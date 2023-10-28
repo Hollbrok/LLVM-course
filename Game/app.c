eb951516f701ca68aedae7cecebe390fd4623121
Hollbrok <talashkevich.da@phystech.edu>
2023-10-28 16:35:08 +0300

    [Game] remove memcpy

--- Game/app.c
+++ Game/app.c
@@ -1,59 +1,55 @@
 
 #include "lib.h"
 
-static int grid[GRID_X][GRID_Y];
-static int grid_buffer[GRID_X][GRID_Y];
-
-static void SwapGrid() {
+static void UpdateGrid(int (*cur)[GRID_X][GRID_Y], int (*next)[GRID_X][GRID_Y]) {
     for (int x = 0; x < GRID_X; ++x) {
         for (int y = 0; y < GRID_Y; ++y) {
-            grid[x][y] = grid_buffer[x][y];
-        }
-    }
-}
-
-static void UpdateGrid() {
-    for (int x = 0; x < GRID_X; ++x) {
-        for (int y = 0; y < GRID_Y; ++y) {
-            int aliveNeighbors = Lib_CountAliveNeighbors(grid, x, y);
-            if (grid[x][y] == 1) {
-                if (aliveNeighbors < 2 || aliveNeighbors > 3) {
-                    grid_buffer[x][y] = 0;
-                }
+            int aliveNeighbors = Lib_CountAliveNeighbors(cur, x, y);
+            if ((*cur)[x][y] == 1) {
+                (*next)[x][y] = !(aliveNeighbors < 2 || aliveNeighbors > 3);
             } else {
-                if (aliveNeighbors == 3) {
-                    grid_buffer[x][y] = 1;
-                }
+                (*next)[x][y] = (aliveNeighbors == 3);
             }
         }
     }
 }
 
-static void DrawGrid() {
+static void DrawGrid(int (*cur_grid)[GRID_X][GRID_Y]) {
     for (int x = 0; x < GRID_X; ++x) {
         for (int y = 0; y < GRID_Y; ++y) {
-            Lib_DrawCell(x, y, grid[x][y]);
+            Lib_DrawCell(x, y, (*cur_grid)[x][y]);
         }
     }
 }
 
-static void InitGrid() {
+static void InitGrid(int (*grid)[GRID_X][GRID_Y], int (*grid_buffer)[GRID_X][GRID_Y]) {
     int cellNumber = Lib_Rand(1000, 5000);
     for (int i = 0; i < cellNumber; ++i) {
         long y = Lib_Rand(0, GRID_X - 1);
         long x = Lib_Rand(0, GRID_Y - 1);
-        grid[x][y] = 1;
-        grid_buffer[x][y] = 0;
+        (*grid)[x][y] = 1;
+        (*grid_buffer)[x][y] = 0;
     }
 }
 
+static void SwapPointers(int **ptr1, int **ptr2) {
+    int *temp = *ptr1;  
+    *ptr1 = *ptr2;      
+    *ptr2 = temp; 
+}
+
 void app() {
-    InitGrid();
+    int grid1[GRID_X][GRID_Y];
+    int grid2[GRID_X][GRID_Y];
+    int *cur = grid1;
+    int *next = grid2;
+    InitGrid(cur, next);
 
     while (1) {
-        UpdateGrid();
-        SwapGrid();
-        DrawGrid();
+        UpdateGrid(cur, next);
+        DrawGrid(next);
         Lib_Display();
+        SwapPointers(&cur, &next);
+        
     }
 }
